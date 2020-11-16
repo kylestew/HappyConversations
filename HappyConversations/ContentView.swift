c6d0d3fe67700a8b0d193b04ae275467179683d3
Kyle Stewart <kyle@polarr.co>
2020-11-16 11:14:33 -0800

    Project setup

---
+++ HappyConversations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HappyConversations
+//
+//  Created by Kyle Stewart on 11/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HappyConversations/HappyConversationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyConversationsApp.swift
+//  HappyConversations
+//
+//  Created by Kyle Stewart on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct HappyConversationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
