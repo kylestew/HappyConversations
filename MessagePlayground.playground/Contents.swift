176e62ea8b4ec80dc677a433c06e891b24868392
Kyle Stewart <kyle@polarr.co>
2020-11-17 15:01:24 -0800

    Wrap up playground and docs!

--- MessagePlayground.playground/Contents.swift
+++ MessagePlayground.playground/Contents.swift
@@ -1,31 +1,37 @@
 import PlaygroundSupport
 import MessageService
 
+/// SERVICE URL - follow along here as you run this playground
+/// https://abraxvasbh.execute-api.us-east-2.amazonaws.com/proto/messages
+
 let service = MessageService()
 
-// post a message
-let message = MessageService.Message(user: "me", subject: "Hello World", message: "Let the conversations begin!")
-let handle = service.post(message) { result in
+// =========================
+// Post a message
+// =========================
+let message = MessageService.Message(user: "billy", subject: "Hello World", message: "Let the conversations begin!")
+let task = service.post(message) { result in
     switch result {
     case .success(let response):
-        print(response.statusCode)
+        let statusCode = response.statusCode
+
+        // =========================
+        // Get all messages
+        // =========================
+        let task = service.fetch { result in
+            switch result {
+            case .success(let response):
+                let message = response.messages
+                let messagesByUser = response.messagesByUser()
+                let myMessages = response.messages(for: "billy")
+
+            default: break
+
+                PlaygroundPage.current.finishExecution()
+            }
+        }
     default: break
     }
-
-    PlaygroundPage.current.finishExecution()
 }
-
-
-// get all messages
-//let bob = service.fetch { result in
-//    switch result {
-//    case .success(let response):
-//        print(response)
-//
-//    case .failure(let error):
-//        assertionFailure()
-//    }
-//
-//}
 
 PlaygroundPage.current.needsIndefiniteExecution = true
